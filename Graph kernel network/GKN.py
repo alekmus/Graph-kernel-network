df6303c559c14995c42911b43e338718175bc304
Aleksi <>
2020-11-18 14:31:08 +0200

    GKNet class partially operational, Lebesque measure required to actually function as GKN instead of a GNN

--- Graph kernel network/GKN.py
+++ Graph kernel network/GKN.py
@@ -2,32 +2,48 @@ import tensorflow as tf
 import tensorflow.keras as tfk
 import spektral
 
-
-class GKNet(tfk.Model):
+class GKNet(tfk.models.Model):
     """[summary]
     """
-    def __init__(self, channels, depth, kernel_layers, inputs, activation=tf.nn.relu):
-        """[summary]
-
-        Args:
-            channels ([type]): [description]
-            depth ([type]): [description]
-            kernel_layers ([type]): [description]
-            activation ([type], optional): [description]. Defaults to tf.nn.relu.
-
-        Returns:
-            [type]: [description]
-        """
-        super(GKNet, self).__init__(inputs=inputs)
+    def __init__(self, 
+                 channels, 
+                 depth, 
+                 kernel_layers, 
+                 adj_matrix, 
+                 edge_features,
+                 n_out,
+                 name = 'Graph_kernel_network',
+                 **kwargs):
         
+        super(GKNet, self).__init__(name=name, **kwargs)
+        self.adj_matrix = adj_matrix
+        self.edge_features = edge_features
         self.depth = depth
-        self.activation = activation
-        self.conv = spektral.layers.EdgeConditionedConv(channels, kernel_layers, activation=tf.nn.relu)
 
+        self.conv_layers = []
+        """
+        self.conv_layers1 = spektral.layers.EdgeConditionedConv(channels, kernel_layers, activation=tf.nn.relu)
+        self.conv_layers2 = spektral.layers.EdgeConditionedConv(channels, kernel_layers, activation=tf.nn.relu)
+        """
+        self.pool = spektral.layers.GlobalSumPool()
+        self.dense = tfk.layers.Dense(n_out)
+        
+        for i in range(depth):
+            self.conv_layers.append(spektral.layers.EdgeConditionedConv(channels, kernel_layers, activation=tf.nn.relu))
+        
 
     def call(self, inputs):
-        node_features, adjacency_matrix, edge_features = inputs
-        for i in range(self.depth):
-            node_features = self.activation(self.conv([node_features, adjacency_matrix, edge_features]))
+        X = inputs[0]
+        A = inputs[1]
+        E = inputs[2]
         
-        return node_features
+        """
+        X = self.conv_layers1([X, A, E])
+        X = self.conv_layers2([X, A, E])
+        X = self.pool(X)
+        return self.dense(X)
+        """
+        for i in range(self.depth):
+            X = self.conv_layers[i]([X, A, E])           
+        return self.dense(self.pool(X))
+       
