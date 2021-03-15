57de2e6e74418f34fe53a590912c694941e5c1d3
Aleksi <>
2021-03-15 08:08:21 +0200

    epochs

--- Graph kernel network/eitnet.py
+++ Graph kernel network/eitnet.py
@@ -37,7 +37,7 @@ def generate_EITNet():
 
 if __name__== '__main__':
     BATCH_SIZE = 1
-    EPOCHS = 250
+    EPOCHS = 1000
     # Load data and convert .mat files if necessary
     data = EIT_dataset('mat_data')
     # Inplace operation
