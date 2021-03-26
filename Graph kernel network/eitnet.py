22b104a29ce0045bb002ce12d319035a63bb2c86
Aleksi <>
2021-03-26 08:42:09 +0200

    normalized conductivity

--- Graph kernel network/eitnet.py
+++ Graph kernel network/eitnet.py
@@ -31,7 +31,7 @@ def masked_MAPE(y_true, y_pred):
 
 def generate_EITNet():
     model = gkn.GKNet(64, 8, [128, 32, 128])
-    optimizer = tfk.optimizers.RMSprop(learning_rate=0.01, centered=True, momentum=0.8)
+    optimizer = tfk.optimizers.RMSprop(learning_rate=0.1, centered=True, momentum=0.8)
     model.compile(optimizer, loss=masked_mse, metrics=[masked_MAPE])
     return model
 
