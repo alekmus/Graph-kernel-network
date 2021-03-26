2c3e653287c0045f084a632a22698f5b284399a1
Aleksi <>
2021-03-26 09:55:28 +0200

    linear out

--- Graph kernel network/GKN.py
+++ Graph kernel network/GKN.py
@@ -25,9 +25,9 @@ class GKNet(tfk.models.Model):
         self.depth = depth
         self.channels = channels
         self.in_layer = spektral.layers.ECCConv(channels, kernel_layers)
-        self.conv_layer = spektral.layers.ECCConv(channels, kernel_layers)
+        self.conv_layer = spektral.layers.ECCConv(channels, kernel_layers, activation=tf.nn.leaky_relu)
         self.norm_layers = [tfk.layers.LayerNormalization() for _ in range(depth)]     
-        self.output_layer = spektral.layers.ECCConv(1, kernel_layers, activation='tanh')
+        self.output_layer = spektral.layers.ECCConv(1, kernel_layers)
         
     
     def call(self, input):
@@ -37,7 +37,6 @@ class GKNet(tfk.models.Model):
         X = self.in_layer([X,A,E])
         for i in range(self.depth):
             X = self.conv_layer([X,A,E])
-            X = tf.nn.leaky_relu(X)
             X = self.norm_layers[i](X)
         X = self.output_layer([X,A,E])
         return X
