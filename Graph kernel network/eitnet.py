9425e1303271252e172fd656b6e1d6281e90d48e
Aleksi <>
2021-03-27 19:58:19 +0200

    optimizer

--- Graph kernel network/eitnet.py
+++ Graph kernel network/eitnet.py
@@ -32,7 +32,7 @@ def masked_MAPE(y_true, y_pred):
 def generate_EITNet():
     width = 1024
     model = gkn.GKNet(64, 7, [width, width])
-    optimizer = tfk.optimizers.RMSprop(learning_rate=0.00001, centered=True, momentum=0.8)
+    optimizer = tfk.optimizers.Adam(learning_rate=0.00001)
     model.compile(optimizer, loss=masked_mse, metrics=[masked_MAPE])
     return model
 
