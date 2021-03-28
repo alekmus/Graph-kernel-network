0538115f6b43d8a50b72e73116fd8d6abe48db59
Aleksi <>
2021-03-28 18:09:30 +0300

    disjoint

--- Graph kernel network/eitnet.py
+++ Graph kernel network/eitnet.py
@@ -47,8 +47,8 @@ if __name__== '__main__':
     val_data = data[:split_i]
     train_data = data[split_i:]
     # Define loader to create minibatches
-    loader = spektral.data.loaders.SingleLoader(train_data[:1])
-    val_loader = utilities.WDJLoader(val_data[:1], batch_size = BATCH_SIZE, node_level=True)
+    loader = utilities.WDJLoader(train_data, batch_size = BATCH_SIZE, node_level=True)
+    val_loader = utilities.WDJLoader(val_data, batch_size = BATCH_SIZE, node_level=True)
    
     model = generate_EITNet()
     
